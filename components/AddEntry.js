40d843398ac521169d9ed8f65ac1d671153312da
Mert <mertdemirok80@gmail.com>
2020-09-05 22:36:31 +0200

    update icon in AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
+import { Ionicons } from "@expo/vector-icons";
+import TextButton from "./TextButton";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -72,8 +74,28 @@ export default class AddEntry extends Component {
 
     // Clear local notification
   };
+
+  reset = () => {
+    const key = timeToString();
+
+    // Update Redux
+
+    // Route to Home
+
+    // Update "DB"
+  };
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (true) {
+      return (
+        <View>
+          <Ionicons name="md-happy" size={100} color="black" />
+          <Text>You already logged your information today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>
