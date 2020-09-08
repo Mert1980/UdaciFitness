8a6c89248bdca2da2fe4862ed1a8ce766327a672
Mert <mertdemirok80@gmail.com>
2020-09-08 20:35:24 +0200

    create History component

---
+++ components/History.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import { receiveEntries, addEntry } from "../actions";
+import { timeToString, getDailyReminderValue } from "../utils/helpers";
+import { fetchCalendarResults } from "../utils/api";
+import entries from "../reducers";
+
+class History extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    // if there is no entry today, we add this info as a property
+    // on our state under the key today
+    fetchCalendarResults()
+      .then((entries) => dispatch(receiveEntries(entries)))
+      .then(({ entries }) => {
+        if (!entries[timeToString()]) {
+          dispatch(
+            addEntry({
+              [timeToString()]: getDailyReminderValue(),
+            })
+          );
+        }
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>{JSON.stringify(this.props)}</Text>
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(entries) {
+  return {
+    entries,
+  };
+}
+
+export default connect(mapStateToProps)(History);
