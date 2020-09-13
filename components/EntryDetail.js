9212011861692a3f9b7e6cd3ecebe98826a4f600
Mert <mertdemirok80@gmail.com>
2020-09-13 10:26:05 +0200

    use setOptions to set title in EntryDetail Comp

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class EntryDetail extends Component {
+  setTitle = (entryId) => {
+    if (!entryId) return;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    this.props.navigation.setOptions({
+      title: `${month}/${day}/${year}`,
+    });
+  };
+
   render() {
+    const { entryId } = this.props.route.params;
+    this.setTitle(entryId);
     return (
       <View>
         <Text>Entry Detail - {this.props.route.params.entryId}</Text>
