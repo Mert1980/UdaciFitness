a6b64ee50e5cf9da15ea0f7d897596ca4a24adf9
Mert <mertdemirok80@gmail.com>
2020-09-15 12:00:55 +0200

    style granted option

--- components/Live.js
+++ components/Live.js
@@ -9,10 +9,10 @@ import {
 import { Foundation } from "@expo/vector-icons";
 import { purple, white } from "../utils/colors";
 
-class Live extends Component {
+export default class Live extends Component {
   state = {
     coords: null,
-    status: "denied",
+    status: "granted",
     direction: "",
   };
 
@@ -50,8 +50,20 @@ class Live extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Text style={styles.direction}>East</Text>
+        </View>
+        <View style={styles.metricContainer}>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Altitude</Text>
+            <Text style={styles.subHeader}>0 feet</Text>
+          </View>
+          <View style={styles.metric}>
+            <Text style={[styles.header, { color: white }]}>Speed</Text>
+            <Text style={styles.subHeader}>0 feet</Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -80,6 +92,38 @@ const styles = StyleSheet.create({
     color: white,
     fontSize: 20,
   },
+  directionContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 35,
+    textAlign: "center",
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: "center",
+  },
+  metricContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: purple,
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: "center",
+    marginTop: 5,
+    color: white,
+  },
 });
-
-export default Live;
