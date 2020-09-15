a90a412c1146b145906c202eaf941633e6af6660
Mert <mertdemirok80@gmail.com>
2020-09-15 19:26:58 +0200

    setup helper functions for local notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,6 +6,10 @@ import {
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
 import { white, black, red, orange, blue, lightPurp, pink } from "./colors";
+import * as Permissions from "expo-permissions";
+import * as Notifications from "expo-notifications";
+
+const NOTIFICATION_KEY = "UdaciFitness:notifications";
 
 const styles = StyleSheet.create({
   iconContainer: {
@@ -146,4 +150,52 @@ export function getDailyReminderValue() {
       today: "👋 Don't forget to log your data today!",
     },
   ];
+}
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
+}
+
+function createNotification() {
+  return {
+    title: "Log your stats!",
+    body: "👋 don't forget to log your stats for today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true,
+    },
+  };
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
+          if (status === "granted") {
+            Notifications.cancelAllScheduledNotificationsAsync();
+
+            let tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            tomorrow.setHours(20);
+            tomorrow.setMinutes(0);
+
+            Notifications.scheduleLocalNotificationAsync(createNotification(), {
+              time: tomorrow,
+              repeat: "day",
+            });
+
+            AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+          }
+        });
+      }
+    });
 }
